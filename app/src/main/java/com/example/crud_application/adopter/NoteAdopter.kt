43cc30abccf7f58acc832b11e2825c3ecbb072c1
IT22544940 <127665117+IT22544940@users.noreply.github.com>
2024-05-12 19:38:44 +0530

    new edit 2

--- app/src/main/java/com/example/crud_application/adopter/NoteAdopter.kt
+++ app/src/main/java/com/example/crud_application/adopter/NoteAdopter.kt
@@ -1,9 +1,12 @@
 package com.example.crud_application.adopter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.crud_application.R
 import com.example.crud_application.database.Note
 import com.example.crud_application.databinding.ItemNoteBinding
 import com.example.crud_application.helper.NoteDiffCallback
@@ -17,10 +20,9 @@ class NoteAdapter(private val listener: NoteItemClickListener): RecyclerView.Ada
         fun onDeleteClick(note: Note)
     }
 
-    class NoteViewHolder(private val binding : ItemNoteBinding,private val listener: NoteItemClickListener): RecyclerView.ViewHolder(binding.root) {
-        fun bind(note: Note){
-            with(binding){
-
+    class NoteViewHolder(private val binding: ItemNoteBinding, private val listener: NoteItemClickListener) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: Note) {
+            with(binding) {
                 tvItemTitle.text = "Title: ${note.title}"
                 tvItemDescription.text = "Description: ${note.description}"
                 tvItemPriority.text = "Priority: ${note.priority}"
@@ -28,7 +30,12 @@ class NoteAdapter(private val listener: NoteItemClickListener): RecyclerView.Ada
                 tvItemDate.text = "Date: ${note.date}"
 
 
-                binding.root.setOnClickListener {
+                val priorityColor = note.priority?.let {
+                    getPriorityColor(it, itemView.context)
+                } ?: ContextCompat.getColor(itemView.context, R.color.default_color)
+                cardView.setCardBackgroundColor(priorityColor)
+
+                root.setOnClickListener {
                     listener.onItemClick(note)
                 }
 
@@ -41,10 +48,19 @@ class NoteAdapter(private val listener: NoteItemClickListener): RecyclerView.Ada
                 }
             }
         }
+
+        private fun getPriorityColor(priority: String, context: Context): Int {
+            return when (priority) {
+                "High Priority" -> R.color.high_priority_color
+                "Medium Priority" -> R.color.medium_priority_color
+                "Low Priority" -> R.color.low_priority_color
+                else -> R.color.default_color
+            }.let { ContextCompat.getColor(context, it) }
+        }
     }
 
-    fun setListNotes(listNotes: List<Note>){
-        val diffCallback = NoteDiffCallback(this.listNotes,listNotes)
+    fun setListNotes(listNotes: List<Note>) {
+        val diffCallback = NoteDiffCallback(this.listNotes, listNotes)
         val diffResult = DiffUtil.calculateDiff(diffCallback)
         this.listNotes.clear()
         this.listNotes.addAll(listNotes)
@@ -52,13 +68,11 @@ class NoteAdapter(private val listener: NoteItemClickListener): RecyclerView.Ada
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        val binding = ItemNoteBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return NoteViewHolder(binding,listener)
+        val binding = ItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoteViewHolder(binding, listener)
     }
 
-    override fun getItemCount(): Int {
-        return listNotes.size
-    }
+    override fun getItemCount(): Int = listNotes.size
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         holder.bind(listNotes[position])
